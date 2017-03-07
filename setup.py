8a80570c7fff03955a7a22ac86d3aa6b49e79904
davidtnfsh <davidtnfsh@gmail.com>
2017-03-07 20:50:01 +0800

    update version num

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 setup(
     name = 'djangoApiDec',
     packages = ['djangoApiDec'],
-    version = '1.5',
+    version = '1.6',
     description = 'Convenient python decorator usually used for building api',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/Stufinite/djangoApiDec',
-    download_url = 'https://github.com/Stufinite/djangoApiDec/archive/v1.5.tar.gz',
+    download_url = 'https://github.com/Stufinite/djangoApiDec/archive/v1.6.tar.gz',
     keywords = ['django', 'decorator', 'api'],
     classifiers = [],
     license='MIT',
     install_requires=[
-        'django==1.10.3',
+        'django',
         'requests',
         'simplejson'
     ],
